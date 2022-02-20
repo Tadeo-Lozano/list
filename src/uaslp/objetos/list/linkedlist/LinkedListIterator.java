c03e257d062c8827650312094feacadfa7bf4975
Tadeo <tadeolozanog2@gmail.com>
2022-02-19 18:24:43 -0600

    Ahora si es la actualizacion de los archivos, se actualizo node, linkedList y linkedListIterator

--- src/uaslp/objetos/list/linkedlist/LinkedListIterator.java
+++ src/uaslp/objetos/list/linkedlist/LinkedListIterator.java
@@ -1,8 +1,22 @@
 package uaslp.objetos.list.linkedlist;
 
 public class LinkedListIterator {
-    public boolean hasNext(){
-        return true;
+
+    private Node currentNode;
+
+    LinkedListIterator(Node startNode) {
+        currentNode = startNode;
+    }
+
+    public boolean hasNext() {
+        return currentNode != null;
+    }
+
+    public String next() {
+        String data = currentNode.data;
+
+        currentNode = currentNode.next;
+
+        return data;
     }
 }
-
--- src/uaslp/objetos/list/linkedlist/Node.java
+++ src/uaslp/objetos/list/linkedlist/Node.java
@@ -1,7 +1,18 @@
 package uaslp.objetos.list.linkedlist;
 
-class Node { //Solo pueden ser publicas o package private
+//Modificador de acceso
+
+// El soporte a el pilar: Encapsulación
+// private
+// public
+// package-private / default
+
+class Node {//  -> Las clases solo pueden ser públicas o package-private
     String data;
     Node next;
     Node previous;
+
+    Node(String data) {
+        this.data = data;
+    }
 }
